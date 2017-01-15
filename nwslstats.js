92cd77a2bf577a0b3d78f48378cd867945163308
Elisabeth Fillmore <Elisabeth Fillmore>
2017-01-15 10:52:02 -0800

    functioning getEloScore

--- nwslstats.js
+++ nwslstats.js
@@ -1,24 +1,21 @@
-var nwslTeam = function(teamName, eloScore){
-  this.teamName = teamName;
-  this.eloScore = eloScore;
-};
+var bostonBreakers = {name: "Boston Breakers", eloScore: 1500};
+var chicagoRedStars = {name: "Chicago Red Stars", eloScore: 1500};
+var kansasCity = {name:"Kansas City", eloScore: 1500};
 
-var DataPoint = function(homeTeam, awayTeam, homeElo, awayElo, winner){
-    this.homeTeam = userName;
-    this.awayTeam = rodentType;
-    this.homeElo = observeMonth;
-    this.awayElo = lat;
-    this.winner = 1;
-};
-
-function scores(teamOne, teamTwo, currentK, result) {
-    functionTeamOne = NintyOneWC[teamOne];
-    functionTeamTwo = NintyOneWC[teamTwo];
-    var EloDifference = functionTeamOne - functionTeamTwo;
+// var DataPoint = scores(homeTeam, awayTeam, currentK, homeResult, awayResult){
+// };
+//
+// DataPoint (kansasCity, portlandThorns, 1500, 1500, 0.5)
+//
+function scores(homeTeam, awayTeam, currentK, homeResult, awayResult) {
+    homeTeamElo = homeTeam.eloScore;
+    awayTeamElo = awayTeam.eloScore;
+    var EloDifference = homeTeamElo - awayTeamElo;
     EloDifference = Math.abs(EloDifference);
     var percentage = 1 / (1 + Math.pow(10, EloDifference / 400));
-    var outCome = Math.round(currentK * (result - percentage));
-    NintyOneWC[teamOne]= NintyOneWC[teamOne]+outCome;
-    return document.write(NintyOneWC[teamOne]);
-//    document.ratingchange.percent.value = Math.round(percentage * 100) + "%";
+    var homeOutCome = Math.round(currentK * (homeResult - percentage));
+    var awayOutCome = Math.round(currentK * (awayResult - percentage));
+    homeTeam.eloScore= homeTeamElo+homeOutCome;
+    awayTeam.eloScore= awayTeamElo+awayOutCome;
+    return document.write("postmatch Eloscores are"+homeTeam.eloScore+"and also"+awayTeam.eloScore);
 }
