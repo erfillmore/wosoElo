4627fff1ab043f06d43c0159ca4737aeb0b0127b
Elisabeth Fillmore <Elisabeth Fillmore>
2017-02-01 19:35:38 -0800

    changed k variable and added hometeam advantage

--- 2013nwsldata.js
+++ 2013nwsldata.js
@@ -1,152 +1,155 @@
-var gameMonthApril13 = [scores(kansasCity, portlandThorns, 0.5, 0.5),
-    scores(chicagoRedStars, seattleReign, 0.5, 0.5),
+var nwsl2013Season = [gameMonthApril13/*,gameMonthMay13,gameMonthJune13,gameMonthJuly13,gameMonthAugust13*/]
+
+var gameMonthApril13 = [
+    scores(kansasCity, portlandThorns, 1, 1),
+    scores(chicagoRedStars, seattleReign, 1, 1),
     scores(skyBlue, westernNYFlash,1,0),
-    scores(bostonBreakers,washingtonSpirit,0.5,0.5),
-    scores(washingtonSpirit,westernNYFlash,0.5,0.5),
-    scores(portlandThorns,seattleReign,1,0),
-    scores(kansasCity,seattleReign,1,0),
-    scores(washingtonSpirit,skyBlue,0,1),
-    scores(chicagoRedStars,portlandThorns,0,1),
-    scores(westernNYFlash,bostonBreakers,0,1)
+    scores(bostonBreakers,washingtonSpirit,1,1),
+    scores(washingtonSpirit,westernNYFlash,1,1),
+    scores(portlandThorns,seattleReign,2,1),
+    scores(kansasCity,seattleReign,2,0),
+    scores(washingtonSpirit,skyBlue,1,2),
+    scores(chicagoRedStars,portlandThorns,0,2),
+    scores(westernNYFlash,bostonBreakers,1,2)
 ];
+//
+// var gameMonthMay13 = [
+//   scores(westernNYFlash,skyBlue,1,0),
+//   scores(bostonBreakers,chicagoRedStars,1,0),
+//   scores(washingtonSpirit,portlandThorns,0,1),
+//   scores(seattleReign,kansasCity,0,1),
+//   scores(skyBlue,chicagoRedStars,0.5,0.5),
+//   scores(washingtonSpirit,bostonBreakers,0.5,0.5),
+//   scores(skyBlue,seattleReign,1,0),
+//   scores(westernNYFlash,kansasCity,1,0),
+//   scores(chicagoRedStars,portlandThorns,0,1),
+//   scores(seattleReign,washingtonSpirit,0,1),
+//   scores(portlandThorns,washingtonSpirit,1,0),
+//   scores(seattleReign,skyBlue,0,1),
+//   scores(westernNYFlash,chicagoRedStars,1,0),
+//   scores(bostonBreakers,washingtonSpirit,1,0),
+//   scores(seattleReign,portlandThorns,0,1),
+//   scores(kansasCity,skyBlue,0,1)
+// ];
+//
+// var gameMonthJune13 = [
+//   scores(portlandThorns,chicagoRedStars,0,1),
+//   scores(skyBlue,bostonBreakers,1,0),
+//   scores(bostonBreakers,westernNYFlash,0.5,0.5),
+//   scores(portlandThorns,kansasCity,1,0),
+//   scores(skyBlue,westernNYFlash,0,1),
+//   scores(chicagoRedStars,bostonBreakers,1,0),
+//   scores(seattleReign,kansasCity,0,1),
+//   scores(chicagoRedStars,kansasCity,0,1),
+//   scores(washingtonSpirit,westernNYFlash,0,1),
+//   scores(portlandThorns,seattleReign,1,0),
+//   scores(kansasCity,chicagoRedStars,0,1),
+//   scores(bostonBreakers,skyBlue,0,1),
+//   scores(chicagoRedStars,westernNYFlash,0.5,0.5),
+//   scores(skyBlue,portlandThorns,0.5,0.5),
+//   scores(kansasCity,washingtonSpirit,1,0),
+//   scores(westernNYFlash,seattleReign,0.5,0.5),
+//   scores(bostonBreakers,seattleReign,0,1),
+//   scores(skyBlue,kansasCity,0.5,0.5),
+//   scores(westernNYFlash,washingtonSpirit,1,0),
+//   scores(seattleReign,chicagoRedStars,1,0),
+//   scores(kansasCity,portlandThorns,1,0),
+//   scores(bostonBreakers,skyBlue,1,0)
+// ];
+//
+// var gameMonthJuly13 = [
+//   scores(seattleReign,bostonBreakers,0.5,0.5),
+//   scores(washingtonSpirit,bostonBreakers,0.5,0.5),
+//   scores(chicagoRedStars,westernNYFlash,1,0),
+//   scores(skyBlue,washingtonSpirit,1,0),
+//   scores(portlandThorns,bostonBreakers,0,1),
+//   scores(kansasCity,westernNYFlash,0.5,0.5),
+//   scores(washingtonSpirit,chicagoRedStars,0,1),
+//   scores(seattleReign,westernNYFlash,1,0),
+//   scores(skyBlue,bostonBreakers,0.5,0.5),
+//   scores(portlandThorns,westernNYFlash,0.5,0.5),
+//   scores(chicagoRedStars,kansasCity,0.5,0.5),
+//   scores(seattleReign,washingtonSpirit,1,0),
+//   scores(chicagoRedStars,washingtonSpirit,1,0),
+//   scores(kansasCity,seattleReign,1,0),
+//   scores(westernNYFlash,skyBlue,1,0),
+//   scores(bostonBreakers,portlandThorns,0,1),
+//   scores(kansasCity,bostonBreakers,1,0),
+//   scores(seattleReign,chicagoRedStars,1,0),
+//   scores(washingtonSpirit,bostonBreakers,0,1),
+//   scores(portlandThorns,chicagoRedStars,0.5,0.5),
+//   scores(skyBlue,kansasCity,0,1),
+//   scores(westernNYFlash,washingtonSpirit,1,0),
+//   scores(portlandThorns,skyBlue,1,0)
+// ];
+//
+// var gameMonthAugust13 = [
+//   scores(bostonBreakers,westernNYFlash,0.5,0.5),
+//   scores(chicagoRedStars,seattleReign,1,0),
+//   scores(skyBlue, washingtonSpirit,1,0),
+//   scores(portlandThorns,kansasCity,0,1),
+//   scores(bostonBreakers,portlandThorns,1,0),
+//   scores(westernNYFlash,seattleReign,1,0),
+//   scores(washingtonSpirit,chicagoRedStars,1,0),
+//   scores(bostonBreakers,kansasCity,1,0),
+//   scores(chicagoRedStars,skyBlue,0.5,0.5),
+//   scores(washingtonSpirit,seattleReign,1,0),
+//   scores(westernNYFlash,portlandThorns,0.5,0.5),
+//   scores(westernNYFlash,bostonBreakers,1,0),
+//   scores(seattleReign,portlandThorns,0,1),
+//   scores(kansasCity,chicagoRedStars,0,1),
+//   scores(washingtonSpirit,skyBlue,0.5,0.5)
+// ];
+//
+// var postSeason13 = [
+//   scores(kansasCity,portlandThorns,0,1),
+//   scores(westernNYFlash,skyBlue,1,0)
+// ]
 
-var gameMonthMay13 = [
-  scores(westernNYFlash,skyBlue,1,0),
-  scores(bostonBreakers,chicagoRedStars,1,0),
-  scores(washingtonSpirit,portlandThorns,0,1),
-  scores(seattleReign,kansasCity,0,1),
-  scores(skyBlue,chicagoRedStars,0.5,0.5),
-  scores(washingtonSpirit,bostonBreakers,0.5,0.5),
-  scores(skyBlue,seattleReign,1,0),
-  scores(westernNYFlash,kansasCity,1,0),
-  scores(chicagoRedStars,portlandThorns,0,1),
-  scores(seattleReign,washingtonSpirit,0,1),
-  scores(portlandThorns,washingtonSpirit,1,0),
-  scores(seattleReign,skyBlue,0,1),
-  scores(westernNYFlash,chicagoRedStars,1,0),
-  scores(bostonBreakers,washingtonSpirit,1,0),
-  scores(seattleReign,portlandThorns,0,1),
-  scores(kansasCity,skyBlue,0,1)
-];
-
-var gameMonthJune13 = [
-  scores(portlandThorns,chicagoRedStars,0,1),
-  scores(skyBlue,bostonBreakers,1,0),
-  scores(bostonBreakers,westernNYFlash,0.5,0.5),
-  scores(portlandThorns,kansasCity,1,0),
-  scores(skyBlue,westernNYFlash,0,1),
-  scores(chicagoRedStars,bostonBreakers,1,0),
-  scores(seattleReign,kansasCity,0,1),
-  scores(chicagoRedStars,kansasCity,0,1),
-  scores(washingtonSpirit,westernNYFlash,0,1),
-  scores(portlandThorns,seattleReign,1,0),
-  scores(kansasCity,chicagoRedStars,0,1),
-  scores(bostonBreakers,skyBlue,0,1),
-  scores(chicagoRedStars,westernNYFlash,0.5,0.5),
-  scores(skyBlue,portlandThorns,0.5,0.5),
-  scores(kansasCity,washingtonSpirit,1,0),
-  scores(westernNYFlash,seattleReign,0.5,0.5),
-  scores(bostonBreakers,seattleReign,0,1),
-  scores(skyBlue,kansasCity,0.5,0.5),
-  scores(westernNYFlash,washingtonSpirit,1,0),
-  scores(seattleReign,chicagoRedStars,1,0),
-  scores(kansasCity,portlandThorns,1,0),
-  scores(bostonBreakers,skyBlue,1,0)
-];
-
-var gameMonthJuly13 = [
-  scores(seattleReign,bostonBreakers,0.5,0.5),
-  scores(washingtonSpirit,bostonBreakers,0.5,0.5),
-  scores(chicagoRedStars,westernNYFlash,1,0),
-  scores(skyBlue,washingtonSpirit,1,0),
-  scores(portlandThorns,bostonBreakers,0,1),
-  scores(kansasCity,westernNYFlash,0.5,0.5),
-  scores(washingtonSpirit,chicagoRedStars,0,1),
-  scores(seattleReign,westernNYFlash,1,0),
-  scores(skyBlue,bostonBreakers,0.5,0.5),
-  scores(portlandThorns,westernNYFlash,0.5,0.5),
-  scores(chicagoRedStars,kansasCity,0.5,0.5),
-  scores(seattleReign,washingtonSpirit,1,0),
-  scores(chicagoRedStars,washingtonSpirit,1,0),
-  scores(kansasCity,seattleReign,1,0),
-  scores(westernNYFlash,skyBlue,1,0),
-  scores(bostonBreakers,portlandThorns,0,1),
-  scores(kansasCity,bostonBreakers,1,0),
-  scores(seattleReign,chicagoRedStars,1,0),
-  scores(washingtonSpirit,bostonBreakers,0,1),
-  scores(portlandThorns,chicagoRedStars,0.5,0.5),
-  scores(skyBlue,kansasCity,0,1),
-  scores(westernNYFlash,washingtonSpirit,1,0),
-  scores(portlandThorns,skyBlue,1,0)
-];
-
-var gameMonthAugust13 = [
-  scores(bostonBreakers,westernNYFlash,0.5,0.5),
-  scores(chicagoRedStars,seattleReign,1,0),
-  scores(skyBlue, washingtonSpirit,1,0),
-  scores(portlandThorns,kansasCity,0,1),
-  scores(bostonBreakers,portlandThorns,1,0),
-  scores(westernNYFlash,seattleReign,1,0),
-  scores(washingtonSpirit,chicagoRedStars,1,0),
-  scores(bostonBreakers,kansasCity,1,0),
-  scores(chicagoRedStars,skyBlue,0.5,0.5),
-  scores(washingtonSpirit,seattleReign,1,0),
-  scores(westernNYFlash,portlandThorns,0.5,0.5),
-  scores(westernNYFlash,bostonBreakers,1,0),
-  scores(seattleReign,portlandThorns,0,1),
-  scores(kansasCity,chicagoRedStars,0,1),
-  scores(washingtonSpirit,skyBlue,0.5,0.5)
-];
-
-var postSeason13 = [
-  scores(kansasCity,portlandThorns,0,1),
-  scores(westernNYFlash,skyBlue,1,0)
-]
-
-var gameMonthApril14 = [
-  scores(houstonDash, portlandThorns, 0,1),
-  scores(kansasCity,skyBlue,0.5,0.5),
-  scores(washingtonSpirit,westernNYFlash,0,1),
-  scores(seattleReign,bostonBreakers,1,0),
-  scores(washingtonSpirit,kansasCity,1,0),
-  scores(chicagoRedStars,westernNYFlash,1,0),
-  scores(skyBlue,portlandThorns,0.5,0.5),
-  scores(bostonBreakers,houstonDash,0,1),
-  scores(seattleReign,washingtonSpirit,1,0),
-  scores(chicagoRedStars,washingtonSpirit,0,1),
-  scores(portlandThorns,kansasCity,1,0),
-  scores(bostonBreakers,skyBlue,1,0),
-  scores(seattleReign,houstonDash,1,0),
-  scores(skyBlue,seattleReign,0,1),
-  scores(kansasCity,chicagoRedStars,1,0)
-]
-
-var gameMonthMay14 = [
-  scores(washingtonSpirit,seattleReign,0.5,0.5),
-  scores(westernNYFlash, portlandThorns,0.5,0.5),
-  scores(skyBlue,bostonBreakers,1,0),
-  scores(houstonDash,kansasCity,0,1),
-  scores(westernNYFlash, kansasCity,1,0),
-  scores(skyBlue,chicagoRedStars,0.5,0.5),
-  scores(portlandThorns,seattleReign,0,1),
-  scores(westernNYFlash,skyBlue,1,0),
-  scores(chicagoRedStars,houstonDash,1,0),
-  scores(kansasCity,washingtonSpirit,1,0),
-  scores(houstonDash,portlandThorns,0,1),
-  scores(seattleReign,kansasCity,1,0),
-  scores(bostonBreakers,chicagoRedStars,0,1),
-  scores(washingtonSpirit,westernNYFlash,1,0),
-  scores(kansasCity,houstonDash,0.5,0.5),
-  scores(bostonBreakers,chicagoRedStars,0,1),
-  scores(washingtonSpirit,skyBlue,0.5,0.5),
-  scores(kansasCity,seattleReign,0.5,0.5),
-  scores(portlandThorns,westernNYFlash,1,0),
-  scores(houstonDash,chicagoRedStars,0,1),
-  scores(portlandThorns,skyBlue,0,1),
-  scores(bostonBreakers,kansasCity,0,1),
-  scores(seattleReign,westernNYFlash,0.5,0.5),
-  scores(washingtonSpirit,houstonDash,1,0),
-  scores(skyBlue,chicagoRedStars,0,1),
-  scores(bostonBreakers,portlandThorns,1,0),
-  scores(westernNYFlash,houstonDash,0,1)
-]
+// var gameMonthApril14 = [
+//   scores(houstonDash, portlandThorns, 0,1),
+//   scores(kansasCity,skyBlue,0.5,0.5),
+//   scores(washingtonSpirit,westernNYFlash,0,1),
+//   scores(seattleReign,bostonBreakers,1,0),
+//   scores(washingtonSpirit,kansasCity,1,0),
+//   scores(chicagoRedStars,westernNYFlash,1,0),
+//   scores(skyBlue,portlandThorns,0.5,0.5),
+//   scores(bostonBreakers,houstonDash,0,1),
+//   scores(seattleReign,washingtonSpirit,1,0),
+//   scores(chicagoRedStars,washingtonSpirit,0,1),
+//   scores(portlandThorns,kansasCity,1,0),
+//   scores(bostonBreakers,skyBlue,1,0),
+//   scores(seattleReign,houstonDash,1,0),
+//   scores(skyBlue,seattleReign,0,1),
+//   scores(kansasCity,chicagoRedStars,1,0)
+// ]
+//
+// var gameMonthMay14 = [
+//   scores(washingtonSpirit,seattleReign,0.5,0.5),
+//   scores(westernNYFlash, portlandThorns,0.5,0.5),
+//   scores(skyBlue,bostonBreakers,1,0),
+//   scores(houstonDash,kansasCity,0,1),
+//   scores(westernNYFlash, kansasCity,1,0),
+//   scores(skyBlue,chicagoRedStars,0.5,0.5),
+//   scores(portlandThorns,seattleReign,0,1),
+//   scores(westernNYFlash,skyBlue,1,0),
+//   scores(chicagoRedStars,houstonDash,1,0),
+//   scores(kansasCity,washingtonSpirit,1,0),
+//   scores(houstonDash,portlandThorns,0,1),
+//   scores(seattleReign,kansasCity,1,0),
+//   scores(bostonBreakers,chicagoRedStars,0,1),
+//   scores(washingtonSpirit,westernNYFlash,1,0),
+//   scores(kansasCity,houstonDash,0.5,0.5),
+//   scores(bostonBreakers,chicagoRedStars,0,1),
+//   scores(washingtonSpirit,skyBlue,0.5,0.5),
+//   scores(kansasCity,seattleReign,0.5,0.5),
+//   scores(portlandThorns,westernNYFlash,1,0),
+//   scores(houstonDash,chicagoRedStars,0,1),
+//   scores(portlandThorns,skyBlue,0,1),
+//   scores(bostonBreakers,kansasCity,0,1),
+//   scores(seattleReign,westernNYFlash,0.5,0.5),
+//   scores(washingtonSpirit,houstonDash,1,0),
+//   scores(skyBlue,chicagoRedStars,0,1),
+//   scores(bostonBreakers,portlandThorns,1,0),
+//   scores(westernNYFlash,houstonDash,0,1)
+// ]
--- nwslstats.js
+++ nwslstats.js
@@ -1,50 +1,71 @@
-var teamObject = function(name,id) {
+var teamObject = function(name, id) {
     this.name = name;
     this.eloScore = 1500;
     this.id = id
-  }
+}
 
 var nwslTeam = [
-  bostonBreakers = new teamObject("Boston Breakers", "bb"),
-  chicagoRedStars = new teamObject("Chicago Red Stars", "crs"),
-  kansasCity = new teamObject("Kansas City", "kcfc"),
-  portlandThorns = new teamObject("Portland Thorns", "ptfc"),
-  seattleReign = new teamObject("Seattle Reigh", "sr"),
-  skyBlue = new teamObject("Sky Blue", "sb"),
-  washingtonSpirit = new teamObject("Washington Spirit", "ws"),
-  westernNYFlash = new teamObject("Western New York Flash", "wnyf"),
-  houstonDash = new teamObject("Houston Dash", "hd")
+    bostonBreakers = new teamObject("Boston Breakers", "bb"),
+    chicagoRedStars = new teamObject("Chicago Red Stars", "crs"),
+    kansasCity = new teamObject("Kansas City", "kcfc"),
+    portlandThorns = new teamObject("Portland Thorns", "ptfc"),
+    seattleReign = new teamObject("Seattle Reigh", "sr"),
+    skyBlue = new teamObject("Sky Blue", "sb"),
+    washingtonSpirit = new teamObject("Washington Spirit", "ws"),
+    westernNYFlash = new teamObject("Western New York Flash", "wnyf"),
+    houstonDash = new teamObject("Houston Dash", "hd")
 ]
 
 function scores(homeTeam, awayTeam, homeResult, awayResult) {
-    homeTeamElo = homeTeam.eloScore;
+    //modifying kfunction
+    currentK = 25
+    goalDiff = Math.abs(homeResult - awayResult);
+    if (goalDiff < 2) {
+      currentK = 25;
+    } else if (goalDiff == 2) {
+      currentK = currentK*1.5;
+    } else if (goalDiff == 3)  {
+      currentK = currentK*1.75;
+    } else {
+      currentK = (currentK * (0.75 + (goalDiff-3)/8))+ currentK;
+    }
+    // If Else statement so instead of putting in 0 or 1 into the function we can put in the ACTUAL score; have the function figure out who gets the 1 value
+    if (homeResult > awayResult) { //home team score is GREATER than away team score; home=1, away=0
+        homeResult = 1;
+        awayResult = 0;
+    } else if (homeResult < awayResult) { //home team score is LESS than away team score; home=0, away=1
+        homeResult = 0;
+        awayResult = 1;
+    } else { //tie; both get .5
+        homeResult = .5;
+        awayResult = .5;
+    }
+    homeTeamElo = homeTeam.eloScore + 20; //adding homefield advantage
     awayTeamElo = awayTeam.eloScore;
-    currentK = 32;
     var EloDifference = homeTeamElo - awayTeamElo;
     EloDifference = Math.abs(EloDifference);
     var percentage = 1 / (1 + Math.pow(10, EloDifference / 400));
     var homeOutCome = Math.round(currentK * (homeResult - percentage));
     var awayOutCome = Math.round(currentK * (awayResult - percentage));
-    homeTeam.eloScore= homeTeamElo+homeOutCome;
-    awayTeam.eloScore= awayTeamElo+awayOutCome;
+    homeTeam.eloScore = homeTeamElo + homeOutCome;
+    awayTeam.eloScore = awayTeamElo + awayOutCome;
     // return document.write("postmatch eloscores are "+homeTeam.eloScore+" and also "+awayTeam.eloScore+"<br>");
 }
 
-function probability(homeTeam,awayTeam){
-  homeTeamElo = homeTeam.eloScore;
-  awayTeamElo = awayTeam.eloScore;
-  var EloDifference = homeTeamElo - awayTeamElo;
-  EloDifference = Math.abs(EloDifference);
-  var percentage = Math.round((1 / (1 + Math.pow(10, EloDifference / 400)))*1000)/10;
-  return document.write("the chance of "+homeTeam.name+" winning against "+awayTeam.name+" is "+percentage+"% <br>");
+
+function probability(homeTeam, awayTeam) {
+    homeTeamElo = homeTeam.eloScore;
+    awayTeamElo = awayTeam.eloScore;
+    var EloDifference = homeTeamElo - awayTeamElo;
+    EloDifference = Math.abs(EloDifference);
+    var percentage = Math.round((1 / (1 + Math.pow(10, EloDifference / 400))) * 1000) / 10;
+    return document.write("the chance of " + homeTeam.name + " winning against " + awayTeam.name + " is " + percentage + "% <br>");
 }
 
 
-function printScores(month){
-  document.write("At the end of "+month+" NWSL Elo Scores are:<br>");
-  for (i=0; i<nwslTeam.length; i++) {
-    document.write(nwslTeam[i].name+"="+nwslTeam[i].eloScore+"<br>")
-  }
+function printScores(array,month) {
+    document.write("At the end of " + month + " NWSL Elo Scores are:<br>");
+    for (i = 0; i < nwslTeam.length; i++) {
+        document.write(nwslTeam[i].name + "=" + nwslTeam[i].eloScore + "<br>")
+    }
 }
-
-// var gameDayTwo = [probability(washingtonSpirit,westernNYFlash)]
